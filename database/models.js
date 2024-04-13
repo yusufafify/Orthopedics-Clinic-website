826ee4c4797e9fc4f60b78e7d02c55280913748e
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2024-04-13 20:22:28 +0200

    added database folder and new attributes in the mongo model

---
+++ database/models.js
@@ -0,0 +1,32 @@
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://abdullahfouad235:abdullahfouad532@crepezinger.cnpysts.mongodb.net/orthopedic-clinic?retryWrites=true&w=majority&appName=crepeZinger');
+mongoose.connection.once('open', () => {
+  console.log('Connected to MongoDB!');
+})
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  address: String,
+  phoneNumber: String,
+  email: {
+    type: String,
+    required: true,
+    match: /^[^ ]+@[^ ]+\.[a-z]{2,3}$/ // Add the regex pattern for email validation
+  },
+  age: Number,
+  role: { type: String, enum: ['patient', 'staff', 'admin'], required: true }
+});
+
+const Users = mongoose.model('Users', userSchema);
+const userData={
+  name: "Abdullah",
+  address: "Riyadh",
+  phoneNumber: "0555555555",
+  email: "ahahah@gamia.cin",
+  age: 25,
+  role: "patient"
+}
+const user = new Users(userData); // Create a new user in the database    
+user.save(); // Save the user to the database
