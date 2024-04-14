3dbd401c9ac0e8ba323f81eace952cfbb203ccc7
Tarek-Waleed <115494652+Tarek-Waleed@users.noreply.github.com>
2024-04-14 02:41:29 +0200

    implemented jwt

--- server/server.py
+++ server/server.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, render_template, request
 from flask_cors import CORS
 from pymongo.mongo_client import MongoClient
-
+import jwt
+from datetime import datetime, timedelta
+from functools import wraps
 uri = "mongodb+srv://abdullahfouad235:abdullahfouad532@crepezinger.cnpysts.mongodb.net/orthopedic-clinic?retryWrites=true&w=majority&appName=crepeZinger"
 
 # Create a new client and connect to the server
@@ -10,11 +12,34 @@ uri = "mongodb+srv://abdullahfouad235:abdullahfouad532@crepezinger.cnpysts.mongo
 app = Flask(__name__)
 CORS(app)
 client = MongoClient(uri)
-
+app.config['SECRET_KEY'] = '42069sexweed'
 db=client['orthopedic-clinic']
 users = db['users']
 # Roles=['admin','staff','patient']
 
+def token_required(f):
+	@wraps(f)
+	def decorated(*args, **kwargs):
+		token = None
+		# jwt is passed in the request header
+		if 'token' in request.headers:
+			token = request.headers['token']
+		# return 401 if token is not passed
+		if not token:
+			return jsonify({'message' : 'Token is missing !!'}), 401
+
+		try:
+			# decoding the payload to fetch the stored details
+			data = jwt.decode(token, app.config['SECRET_KEY'])
+			current_user = users.find_one({'email': data['email']})
+		except:
+			return jsonify({
+				'message' : 'Token is invalid !!'
+			}), 401
+		# returns the current logged in users context to the routes
+		return f(current_user, *args, **kwargs)
+
+	return decorated
 
 @app.route('/', methods=['GET'])
 def home():
@@ -34,8 +59,14 @@ def login():
         password = data.get('password')
         user = users.find_one({'email': email, 'password': password})
         if user:
+            token = jwt.encode({
+			'email': user["email"],
+            'role': user["role"],
+			'exp' : datetime.utcnow() + timedelta(minutes = 30)
+		}, app.config['SECRET_KEY'])
             return jsonify({
                 'message': 'success',
+                'token': token,
             })
         else:
             return jsonify({
