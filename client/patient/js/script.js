76bbed20bfd62fe9361ecdd3af355bfe6a275d28
Mahmoudd29 <mahmoud.abdelhalim03@eng-st.cu.edu.eg>
2024-04-19 19:39:15 +0200

    ADMIN

--- client/patient/js/script.js
+++ client/patient/js/script.js
@@ -1,30 +1,38 @@
-var sideNav = document.getElementById('side-nav');
-var toggleBtn = document.getElementById('toggle-btn');
-let closeBtn = document.getElementById('close');
+var sideNav = document.getElementById('sidebar');
+var toggleBtn = document.getElementById('sideOpenBtn');
+let closeBtn = document.getElementById('sideCloseBtn');
+let navLinks = document.querySelectorAll('#sidebar a');
+
 let state=false;
 toggleBtn.addEventListener('click', function () {
   if(state){
-    sideNav.classList.add('hidden');
-    sideNav.classList.remove('absolute');
-    sideNav.classList.remove('w-[200px]');
+    sideNav.classList.add('-translate-x-full');
     state=false;}
     else{
-      sideNav.classList.remove('hidden');
-      sideNav.classList.add('absolute');
-      sideNav.classList.add('w-[200px]');
+      sideNav.classList.remove('-translate-x-full');
       state=true;
     }
 });
 closeBtn.addEventListener('click', function () {
   if(state){
-    sideNav.classList.add('hidden');
-    sideNav.classList.remove('absolute');
-    sideNav.classList.remove('w-[200px]');
+    sideNav.classList.add('-translate-x-full');
     state=false;}
     else{
-      sideNav.classList.remove('hidden');
-      sideNav.classList.add('absolute');
-      sideNav.classList.add('w-[200px]');
+      sideNav.classList.remove('-translate-x-full');
+      
       state=true;
     }
+});
+
+// Add event listener to each nav link
+navLinks.forEach((link,index) => {
+  link.addEventListener('click', function() {
+    // Remove the active class from all links
+    navLinks.forEach(link => link.classList.remove('bg-gray-100'));
+
+    // Add the active class to the clicked link
+    if(index!==0 && index!==5){
+      this.classList.add('bg-gray-100');
+    }
+  });
 });
