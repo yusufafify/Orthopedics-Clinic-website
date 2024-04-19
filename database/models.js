baff127f4a5aa9885d93af45e5a5b0bc349774f1
AbdullahAhmed30 <abdullah.attia021@eng-st.cu.edu.eg>
2024-04-19 19:54:24 +0200

    patient login and registration handled

--- database/models.js
+++ database/models.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect('mongodb://localhost:27017');
 mongoose.connection.once('open', () => {
   console.log('Connected to MongoDB!');
 })
--- server/main_server/server.py
+++ server/main_server/server.py
@@ -5,9 +5,10 @@ import jwt
 from datetime import datetime, timedelta
 from functools import wraps
 from auth_middleware import token_required
+import bcrypt
 
-uri = "mongodb+srv://abdullahfouad235:abdullahfouad532@crepezinger.cnpysts.mongodb.net/orthopedic-clinic?retryWrites=true&w=majority&appName=crepeZinger"
-# uri = "mongodb://localhost:27017/"
+# uri = "mongodb+srv://abdullahfouad235:abdullahfouad532@crepezinger.cnpysts.mongodb.net/orthopedic-clinic?retryWrites=true&w=majority&appName=crepeZinger"
+uri = "mongodb://localhost:27017/"
 
 # Create a new client and connect to the server
 
@@ -39,9 +40,11 @@ def login():
         data = request.get_json()
         
         email = data.get('email')
-        password = data.get('password')
-        user = users.find_one({'email': email, 'password': password})
-        if user:
+        password = data.get('password').encode('utf-8')
+
+
+        user = users.find_one({ 'email': email })
+        if user and bcrypt.checkpw(password, user['password']):
             token = jwt.encode({
 			      'email': email,
             'role': user["role"],
@@ -71,33 +74,48 @@ def login():
 @app.route('/register', methods=['POST'])
 def register():
     try:
-      data = request.get_json()
-      email=data.get('email')
-      password=data.get('password')
-      phone=data.get('phone')
-      first_name=data.get('firstName')
-      last_name=data.get('lastName')
-      age=data.get('age')
-      address=data.get('address')
-      gender=data.get('gender')
-      users.insert_one({
-          'email':email,  
-          'password':password,
-          'phoneNumber':phone,
-          'name':first_name+' '+last_name,
-          'role':'patient',
-          'gender': gender,
-          'age':int(age),
-          'address': address
-      })
-      return jsonify({
-              'message':'success',
-          })
+        data = request.get_json()
+        email=data.get('email')
+
+        user = users.find_one({ 'email': email })
+        if user: 
+            return jsonify({ 'error': 'invalid email' }), 409
+
+        password=data.get('password').encode('utf-8')
+        hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
+
+        phone=data.get('phone')
+        first_name=data.get('firstName')
+        last_name=data.get('lastName')
+        age=data.get('age')
+        address=data.get('address')
+        gender=data.get('gender')
+        users.insert_one({
+            'email':email,  
+            'password':hashed_password,
+            'phoneNumber':phone,
+            'name':first_name+' '+last_name,
+            'role':'patient',
+            'gender': gender,
+            'age':int(age),
+            'address': address
+        })
+
+        token = jwt.encode({
+            'email': email,
+            'role': 'patient',
+            'exp': datetime.utcnow() + timedelta(minutes = 1)
+        }, app.config['SECRET_KEY'])
+
+        return jsonify({
+                'message':'success',
+                'token': token.decode('utf-8')
+            })
     except Exception as e:
         return jsonify({
             'message': 'error',
             'error': str(e)
-        })
+    })
 
 
 
@@ -126,6 +144,9 @@ def get_patient_data():
         return jsonify({ 'error': str(err) }), 500
 
 
+# @app.route('/appointment_booking', methods=['GET'])
+# @token_required
+# def appointment_booking():
 
 
 
