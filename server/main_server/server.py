39d81f919a5211b7a9fac9222e7c999168917496
Tarek-Waleed <115494652+Tarek-Waleed@users.noreply.github.com>
2024-04-20 02:17:40 +0200

    refresh token done

--- server/main_server/server.py
+++ server/main_server/server.py
@@ -1,10 +1,10 @@
 from flask import Flask, jsonify, render_template, request, g
 from flask_cors import CORS
 from pymongo.mongo_client import MongoClient
-import jwt
+from flask_jwt_extended import JWTManager,create_access_token,create_refresh_token,jwt_required, get_jwt_identity, get_jwt
 from datetime import datetime, timedelta
 from functools import wraps
-from auth_middleware import token_required
+# from auth_middleware import token_required
 
 uri = "mongodb+srv://abdullahfouad235:abdullahfouad532@crepezinger.cnpysts.mongodb.net/orthopedic-clinic?retryWrites=true&w=majority&appName=crepeZinger"
 # uri = "mongodb://localhost:27017/"
@@ -16,8 +16,10 @@ app = Flask(__name__)
 CORS(app)
 client = MongoClient(uri)
 app.config['SECRET_KEY'] = 'r-DbyQUte22QfwuUYTY0KQ'
+app.config['REFRESH_SECRET_KEY'] = 'r-DbyQUtefufnmfdusaYTY0KQ'
 db=client['orthopedic-clinic']
 users = db['users']
+jwt = JWTManager(app)
 # Roles=['admin','staff','patient']
 
 
@@ -41,14 +43,19 @@ def login():
         password = data.get('password')
         user = users.find_one({'email': email, 'password': password})
         if user:
-            token = jwt.encode({
+            token = create_access_token({
 			      'email': email,
             'role': user["role"],
             'exp' : datetime.utcnow() + timedelta(minutes = 1)
 		}, app.config['SECRET_KEY'])
+            refresh_token = create_refresh_token({
+			      'email': email,
+            'role': user["role"]
+		},expires_delta=timedelta(days=1))
             return jsonify({
                 'message': 'success',
-                'token': token.decode('UTF-8'),
+                'token': token,
+                'refresh_token': refresh_token
                 
             })
         else:
@@ -95,13 +102,23 @@ def register():
             'error': str(e)
         })
 
-
+@app.route('/refresh')
+@jwt_required(refresh=True)
+def TokenRefresh():
+    current_user = get_jwt_identity()
+    print(current_user)
+    expires = timedelta(minutes=1)
+    access_token = create_access_token(identity={'email': current_user['email'], 'role':current_user['role']}, 
+                                       secret_key=app.config['SECRET_KEY'], 
+                                       expires_delta=expires)
+    return jsonify({'access_token': access_token})
 
 @app.route('/personal_data', methods=['GET'])
-@token_required
+@jwt_required()
 def get_patient_data():
     try:
-        email = g.user_data['email']
+        current_user = get_jwt_identity()
+        email = current_user['email']
         user = users.find_one({ 'email': email })
         
         if not user: 
@@ -121,7 +138,38 @@ def get_patient_data():
     except Exception as err:
         return jsonify({ 'error': str(err) }), 500
 
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_payload):
+    return (
+        jsonify({"message": "The token has expired.", "error": "token_expired"}),
+        401,
+    )
 
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return (
+        jsonify(
+            {"message": "Signature verification failed.", "error": "invalid_token"}
+        ),
+        401,
+    )
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return (
+        jsonify(
+            {
+                "description": "Request does not contain an access token.",
+                "error": "authorization_required",
+            }
+        ),
+        401,
+    )
+@jwt.additional_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1:
+        return {"is_admin": True}
+    return {"is_admin": False}
 
 
 
