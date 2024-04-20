0232da00700ef1554cde398d59f5b4526ca5e3fd
AbdullahAhmed30 <abdullah.attia021@eng-st.cu.edu.eg>
2024-04-20 14:36:37 +0200

    ta3deel tarteeb elcode felserver

--- server/main_server/server.py
+++ server/main_server/server.py
@@ -22,6 +22,38 @@ appointment=db['appointments']
 jwt = JWTManager(app)
 # Roles=['admin','staff','patient']
 
+@jwt.expired_token_loader
+def expired_token_callback(jwt_header, jwt_payload):
+    return (
+        jsonify({"message": "The token has expired.", "error": "token_expired"}),
+        401,
+    )
+
+@jwt.invalid_token_loader
+def invalid_token_callback(error):
+    return (
+        jsonify(
+            {"message": "Signature verification failed.", "error": "invalid_token"}
+        ),
+        401,
+    )
+
+@jwt.unauthorized_loader
+def missing_token_callback(error):
+    return (
+        jsonify(
+            {
+                "description": "Request does not contain an access token.",
+                "error": "authorization_required",
+            }
+        ),
+        401,
+    )
+@jwt.additional_claims_loader
+def add_claims_to_jwt(identity):
+    if identity == 1:
+        return {"is_admin": True}
+    return {"is_admin": False}
 
 #a Test Route
 @app.route('/', methods=['GET'])
@@ -155,39 +187,6 @@ def get_patient_data():
 
     except Exception as err:
         return jsonify({ 'error': str(err) }), 500
-
-@jwt.expired_token_loader
-def expired_token_callback(jwt_header, jwt_payload):
-    return (
-        jsonify({"message": "The token has expired.", "error": "token_expired"}),
-        401,
-    )
-
-@jwt.invalid_token_loader
-def invalid_token_callback(error):
-    return (
-        jsonify(
-            {"message": "Signature verification failed.", "error": "invalid_token"}
-        ),
-        401,
-    )
-
-@jwt.unauthorized_loader
-def missing_token_callback(error):
-    return (
-        jsonify(
-            {
-                "description": "Request does not contain an access token.",
-                "error": "authorization_required",
-            }
-        ),
-        401,
-    )
-@jwt.additional_claims_loader
-def add_claims_to_jwt(identity):
-    if identity == 1:
-        return {"is_admin": True}
-    return {"is_admin": False}
 
 
 @app.route('/update_data', methods=['PATCH'])
