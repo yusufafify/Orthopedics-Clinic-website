77a61a9d9831d9a68b4263cbb33c4130b2e63d07
AmrDoma <amrdoma123@outlook.com>
2024-04-25 23:53:52 +0200

    finalized doctor's dashboard (clientside)

--- client/Doctor/js/doctor.js
+++ client/Doctor/js/doctor.js
@@ -3,6 +3,34 @@ function openModal(image) {
   document.getElementById('modal').classList.remove('hidden');
 }
 
+function openFinalizeModal() {
+    document.getElementById('finalizeModal').classList.remove('hidden');
+  }
+
+  function openEditCurrentModal() {
+    document.getElementById('editCurrentModal').classList.remove('hidden');
+  }
+
+  function openCancelCurrentModal() {
+    document.getElementById('cancelCurrentModal').classList.remove('hidden');
+  }
+  
+  window.onclick = function(event) {
+    if (event.target == document.getElementById('finalizeModal')) {
+      document.getElementById('finalizeModal').classList.add('hidden');
+    }
+  }
+  
+  function closeFinalizeModal() {
+    document.getElementById('finalizeModal').classList.add('hidden');
+  }
+  function closeCancelCurrentModal() {
+    document.getElementById('cancelCurrentModal').classList.add('hidden');
+  }
+  function closeEditCurrentModal() {
+    document.getElementById('editCurrentModal').classList.add('hidden');
+  }
+
 window.onclick = function(event) {
   if (event.target == document.getElementById('modal')) {
     document.getElementById('modal').classList.add('hidden');
--- server/main_server/server.py
+++ server/main_server/server.py
@@ -270,5 +270,5 @@ def delete_user():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=8080)
+    app.run(debug=True, port=8008)
 
