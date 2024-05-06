34fba89da629ead376614e5e1fae4cdfd0a620c0
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2024-05-06 20:06:07 +0300

    fixed some bugs

--- client/patient/images/js/images.js
+++ client/patient/images/js/images.js
@@ -23,6 +23,8 @@ function handleButtonClick(event) {
   // event.currentTarget is the button that the event listener is attached to
   let buttonId = event.currentTarget.id;
   let number = buttonId.replace(/^\D+/g, "");
+  
+
 
   if (buttonId.includes("delete")) {
     deleteImage(number);
@@ -31,6 +33,13 @@ function handleButtonClick(event) {
 
 function getImages(category) {
   imgContainer.innerHTML = putImageInTheContainer(imgArray, false);
+  imgContainer.classList.remove(
+    "grid-cols-1",
+    "gap-4",
+    "sm:grid-cols-2",
+    "md:grid-cols-3"
+  );
+  imgContainer.classList.add("place-items-center");
   fetch("http://127.0.0.1:8008/medical_images", {
     method: "POST",
     headers: {
@@ -67,15 +76,18 @@ getImages("all");
 
 xrayBtn.addEventListener("click", () => {
   imgArray = [];
+  initializeDialog();
   getImages("x-ray");
 });
 
 mriBtn.addEventListener("click", () => {
   imgArray = [];
+  initializeDialog();
   getImages("mri");
 });
 
 ctscanBtn.addEventListener("click", () => {
   imgArray = [];
+  initializeDialog();
   getImages("ct");
 });
