1d3c8bde33f7faacaf829219622b3ee7bdbc994e
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2024-05-17 16:20:56 +0300

    forget password script added

---
+++ client/forget_password/js/forgetPassword.js
@@ -0,0 +1,56 @@
+const forgetPasswordForm = document.getElementById('forgetPasswordForm');
+
+
+
+const forgetPassword = async (e) => {
+  e.preventDefault();
+  
+  try {
+    const email = document.getElementById('email').value;
+
+    // Check if the email field is filled
+    if (!email) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Please enter your email address!',
+      });
+      return;
+    }
+
+    const response = await fetch('http://localhost:8008/forget_password', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email })
+    });
+
+    const data = await response.json();
+
+    if (data.reset_token) {
+      localStorage.setItem('reset_token', data.reset_token);
+      console.log(localStorage.getItem('reset_token'));
+      Swal.fire({
+        icon: 'success',
+        title: 'Email Sent',
+        text: 'A password reset email has been sent to your email address.',
+      });
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Failed',
+        text: 'Failed to send the password reset email. Please try again.',
+      });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    Swal.fire({
+      icon: 'error',
+      title: 'Oops...',
+      text: 'Something went wrong. Please try again later.',
+    });
+  }
+}
+
+forgetPasswordForm.addEventListener('submit', forgetPassword);
